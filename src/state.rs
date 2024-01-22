d0f84bf82fc0350eda3c4668bb797853bb9f4ec6
Soham Ghugare <soham.ghugare@gmail.com>
2024-01-23 00:38:03 +0530

    feat: declared struct impl fns

--- src/state.rs
+++ src/state.rs
@@ -1,4 +1,4 @@
-use winit::window::Window;
+use winit::{event::WindowEvent, window::Window};
 
 struct State {
     surface: wgpu::Surface,
@@ -9,4 +9,30 @@ struct State {
     // defining window after surface so it gets dropped after surface
     // ps: window contains unsafe references to surface
     window: Window,
+}
+
+impl State {
+    async fn new(window: Window) -> Self {
+        todo!()
+    }
+
+    pub fn window(&self) -> &Window {
+        &self.window
+    }
+
+    fn resize(&mut self, new_size: winit::dpi::PhysicalSize<u32>) {
+        todo!()
+    }
+
+    fn input(&mut self, event: &WindowEvent) -> bool {
+        todo!()
+    }
+
+    fn update(&mut self) {
+        todo!()
+    }
+
+    fn render(&mut self) -> Result<(), wgpu::SurfaceError> {
+        todo!()
+    }
 }
