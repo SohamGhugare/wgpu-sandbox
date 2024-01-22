bcb296536038021323f694baa388990188429831
Soham Ghugare <soham.ghugare@gmail.com>
2024-01-22 22:04:11 +0530

    feat: added logger toggle for wasm32

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,25 @@
+#[cfg(target_arch="wasm32")]
+use wasm_bindgen::prelude::*;
+
 use winit::{
     event::{Event, WindowEvent},
     event_loop::{ControlFlow, EventLoop},
     window::WindowBuilder,
 };
 
+#[cfg_attr(target_arch="wasm32", wasm_bindgen(start))]
 pub fn run() {
+
+    // toggling logger for wasm32
+    cfg_if::cfg_if! {
+        if #[cfg(target_arch = "wasm32")] {
+            std::panic::set_hook(Box::new(console_error_panic_hook::hook));
+            console_log::init_with_level(log::Level::Warn).expect("Couldn't initialize logger");
+        } else {
+            tracing_subscriber::fmt::init();
+        }
+    }
+
     // defining event loop and window
     let event_loop = EventLoop::new().unwrap();
     let window = WindowBuilder::new().build(&event_loop).unwrap();
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
 use wgpu_sandbox::run;
 
 fn main() {
-    tracing_subscriber::fmt::init();
     run();
 }
