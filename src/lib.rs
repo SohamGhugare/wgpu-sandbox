cd7412bcad90aea32c801508d1a8dcc03264aa3b
Soham Ghugare <soham.ghugare@gmail.com>
2024-01-22 23:32:41 +0530

    feat: added html canvas for web

--- src/lib.rs
+++ src/lib.rs
@@ -27,6 +27,25 @@ pub fn run() {
     // setting control flow
     event_loop.set_control_flow(ControlFlow::Wait);
 
+    #[cfg(target_arch = "wasm32")]
+    {
+        // Winit prevents sizing with CSS, so we have to set
+        // the size manually when on web.
+        use winit::dpi::PhysicalSize;
+        window.set_inner_size(PhysicalSize::new(450, 400));
+        
+        use winit::platform::web::WindowExtWebSys;
+        web_sys::window()
+            .and_then(|win| win.document())
+            .and_then(|doc| {
+                let dst = doc.get_element_by_id("wasm-example")?;
+                let canvas = web_sys::Element::from(window.canvas());
+                dst.append_child(&canvas).ok()?;
+                Some(())
+            })
+            .expect("Couldn't append canvas to document body.");
+    }
+
     // looping the window
     event_loop.run(move |event, elwt| {
         // handling all events
