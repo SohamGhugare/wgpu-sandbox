b8370e41cc7947ad27155ddb1ffb58d2a15c6e97
Soham Ghugare <soham.ghugare@gmail.com>
2026-02-22 04:00:46 +0530

    feat: moved state to a separate module

--- src/main.rs
+++ src/main.rs
@@ -1,213 +1,14 @@
-use std::{borrow::Cow, sync::Arc};
+mod state;
 
-use wgpu::{
-    Adapter, Color, CommandEncoderDescriptor, Device, FragmentState, Instance, InstanceDescriptor,
-    MultisampleState, Operations, PipelineLayoutDescriptor, PrimitiveState, Queue,
-    RenderPassColorAttachment, RenderPassDescriptor, RenderPipelineDescriptor,
-    RequestAdapterOptionsBase, ShaderModuleDescriptor, Surface, TextureFormat,
-    TextureViewDescriptor, VertexState, wgt::DeviceDescriptor,
-};
+use state::State;
+use std::sync::Arc;
+
 use winit::{
     application::ApplicationHandler,
-    dpi::PhysicalSize,
     event::WindowEvent,
     event_loop::{ActiveEventLoop, ControlFlow, EventLoop},
     window::{Window, WindowId},
 };
-
-struct State {
-    window: Arc<Window>,
-    adapter: Adapter,
-    device: Device,
-    queue: Queue,
-    size: PhysicalSize<u32>,
-    surface: Surface<'static>,
-    surface_format: TextureFormat,
-}
-
-impl State {
-    async fn new(window: Arc<Window>) -> State {
-        let instance = Instance::new(&InstanceDescriptor::default());
-
-        let adapter = instance
-            .request_adapter(&RequestAdapterOptionsBase::default())
-            .await
-            .unwrap();
-
-        let (device, queue) = adapter
-            .request_device(&DeviceDescriptor::default())
-            .await
-            .unwrap();
-
-        let size = window.inner_size();
-
-        let surface = instance.create_surface(window.clone()).unwrap();
-        let cap = surface.get_capabilities(&adapter);
-        let surface_format = cap.formats[0];
-
-        let state = State {
-            window,
-            adapter,
-            device,
-            queue,
-            size,
-            surface,
-            surface_format,
-        };
-
-        // Configure the surface for the first time
-        state.configure_surface();
-
-        state
-    }
-
-    fn get_window(&self) -> &Window {
-        &self.window
-    }
-
-    fn configure_surface(&self) {
-        // let surface_config = SurfaceConfiguration {
-        //     usage: TextureUsages::RENDER_ATTACHMENT,
-        //     format: self.surface_format,
-        //     width: self.size.width,
-        //     height: self.size.height,
-        //     present_mode: PresentMode::AutoVsync,
-        //     desired_maximum_frame_latency: 2,
-        //     alpha_mode: CompositeAlphaMode::Auto,
-        //     view_formats: vec![self.surface_format.add_srgb_suffix()],
-        // };
-        let config = self
-            .surface
-            .get_default_config(&self.adapter, self.size.width, self.size.height)
-            .unwrap();
-
-        self.surface.configure(&self.device, &config);
-    }
-
-    fn resize(&mut self, new_size: PhysicalSize<u32>) {
-        self.size = new_size;
-    }
-
-    // Renders a blank screen
-    fn _render_blank(&mut self) {
-        // Create texture view
-        let surface_texture = self
-            .surface
-            .get_current_texture()
-            .expect("Failed to acquire next swapchain texture");
-
-        let texture_view = surface_texture.texture.create_view(&TextureViewDescriptor {
-            format: Some(self.surface_format.add_srgb_suffix()),
-            ..Default::default()
-        });
-
-        // Renders a Green screen
-        let mut encoder = self.device.create_command_encoder(&Default::default());
-        // Create the renderpass that will clear the screen
-        let renderpass = encoder.begin_render_pass(&RenderPassDescriptor {
-            label: None,
-            color_attachments: &[Some(RenderPassColorAttachment {
-                view: &texture_view,
-                depth_slice: None,
-                resolve_target: None,
-                ops: Operations {
-                    load: wgpu::LoadOp::Clear(Color::WHITE),
-                    store: wgpu::StoreOp::Store,
-                },
-            })],
-            depth_stencil_attachment: None,
-            timestamp_writes: None,
-            occlusion_query_set: None,
-        });
-
-        // Drawing commands go here
-
-        // End the renderpass
-        drop(renderpass);
-
-        // Submit the command in the queue to execute
-        self.queue.submit([encoder.finish()]);
-        self.window.pre_present_notify();
-        surface_texture.present();
-    }
-
-    fn render_triangle(&mut self) {
-        let shader = &self.device.create_shader_module(ShaderModuleDescriptor {
-            label: None,
-            source: wgpu::ShaderSource::Wgsl(Cow::Borrowed(include_str!("triangle_shader.wgsl"))),
-        });
-
-        let pipeline_layout = &self
-            .device
-            .create_pipeline_layout(&PipelineLayoutDescriptor {
-                label: None,
-                bind_group_layouts: &[],
-                push_constant_ranges: &[],
-            });
-
-        let render_pipeline = &self
-            .device
-            .create_render_pipeline(&RenderPipelineDescriptor {
-                label: None,
-                layout: Some(&pipeline_layout),
-                vertex: VertexState {
-                    module: &shader,
-                    entry_point: Some("vs_main"),
-                    compilation_options: Default::default(),
-                    buffers: &[],
-                },
-                primitive: PrimitiveState::default(),
-                depth_stencil: None,
-                multisample: MultisampleState::default(),
-                fragment: Some(FragmentState {
-                    module: &shader,
-                    entry_point: Some("fs_main"),
-                    compilation_options: Default::default(),
-                    targets: &[Some(self.surface_format.into())],
-                }),
-                multiview: None,
-                cache: None,
-            });
-
-        // self.configure_surface();
-
-        let frame = self
-            .surface
-            .get_current_texture()
-            .expect("Failed to acquire next swapchain texture");
-
-        let view = frame.texture.create_view(&TextureViewDescriptor::default());
-
-        let mut encoder = self
-            .device
-            .create_command_encoder(&CommandEncoderDescriptor { label: None });
-
-        {
-            let mut rpass = encoder.begin_render_pass(&RenderPassDescriptor {
-                label: None,
-                color_attachments: &[Some(RenderPassColorAttachment {
-                    view: &view,
-                    depth_slice: None,
-                    resolve_target: None,
-                    ops: Operations {
-                        load: wgpu::LoadOp::Clear(Color::WHITE),
-                        store: wgpu::StoreOp::Store,
-                    },
-                })],
-                depth_stencil_attachment: None,
-                timestamp_writes: None,
-                occlusion_query_set: None,
-            });
-
-            rpass.set_pipeline(&render_pipeline);
-            rpass.draw(0..3, 0..1);
-        }
-
-        self.queue.submit(Some(encoder.finish()));
-        self.window.pre_present_notify();
-        frame.present();
-    }
-}
 
 #[derive(Default)]
 struct App {
@@ -231,7 +32,7 @@ impl ApplicationHandler for App {
     fn window_event(
         &mut self,
         event_loop: &ActiveEventLoop,
-        window_id: WindowId,
+        _window_id: WindowId,
         event: WindowEvent,
     ) {
         let state = self.state.as_mut().unwrap();
