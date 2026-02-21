0d45765af03611c1a274c09df9732b65a5f86120
Soham Ghugare <soham.ghugare@gmail.com>
2026-02-22 03:46:25 +0530

    feat: added render_triangle fn

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,44 @@
-use std::{default, sync::Arc};
+use std::{borrow::Cow, default, sync::Arc};
 
-use wgpu::{Color, CompositeAlphaMode, Device, Instance, InstanceDescriptor, Operations, PresentMode, Queue, RenderPassColorAttachment, RenderPassDescriptor, RequestAdapterOptionsBase, Surface, SurfaceConfiguration, TextureFormat, TextureUsages, wgt::{DeviceDescriptor, TextureViewDescriptor}};
-use winit::{application::ApplicationHandler, dpi::PhysicalSize, event::WindowEvent, event_loop::{ActiveEventLoop, ControlFlow, EventLoop, OwnedDisplayHandle}, window::{Window, WindowId}};
+use wgpu::{
+    Adapter, Color, CommandEncoderDescriptor, CompositeAlphaMode, Device, FragmentState, Instance,
+    InstanceDescriptor, MultisampleState, Operations, PipelineLayoutDescriptor, PresentMode,
+    PrimitiveState, Queue, RenderPassColorAttachment, RenderPassDescriptor,
+    RenderPipelineDescriptor, RequestAdapterOptionsBase, ShaderModule, ShaderModuleDescriptor,
+    Surface, SurfaceConfiguration, TextureFormat, TextureUsages, TextureViewDescriptor,
+    VertexState, wgt::DeviceDescriptor,
+};
+use winit::{
+    application::ApplicationHandler,
+    dpi::PhysicalSize,
+    event::WindowEvent,
+    event_loop::{ActiveEventLoop, ControlFlow, EventLoop, OwnedDisplayHandle},
+    window::{Window, WindowId},
+};
 
 struct State {
     window: Arc<Window>,
+    adapter: Adapter,
     device: Device,
     queue: Queue,
     size: PhysicalSize<u32>,
     surface: Surface<'static>,
-    surface_format: TextureFormat
+    surface_format: TextureFormat,
 }
 
 impl State {
     async fn new(window: Arc<Window>) -> State {
-        let instance = Instance::new(
-            &InstanceDescriptor::default(),
-        );
+        let instance = Instance::new(&InstanceDescriptor::default());
 
         let adapter = instance
             .request_adapter(&RequestAdapterOptionsBase::default())
-            .await.unwrap();
+            .await
+            .unwrap();
 
         let (device, queue) = adapter
             .request_device(&DeviceDescriptor::default())
-            .await.unwrap();
+            .await
+            .unwrap();
 
         let size = window.inner_size();
 
@@ -34,11 +48,12 @@ impl State {
 
         let state = State {
             window,
+            adapter,
             device,
             queue,
             size,
             surface,
-            surface_format
+            surface_format,
         };
 
         // Configure the surface for the first time
@@ -52,18 +67,22 @@ impl State {
     }
 
     fn configure_surface(&self) {
-        let surface_config = SurfaceConfiguration {
-            usage: TextureUsages::RENDER_ATTACHMENT,
-            format: self.surface_format,
-            width: self.size.width,
-            height: self.size.height,
-            present_mode: PresentMode::AutoVsync,
-            desired_maximum_frame_latency: 2,
-            alpha_mode: CompositeAlphaMode::Auto,
-            view_formats: vec![self.surface_format.add_srgb_suffix()],
-        };
+        // let surface_config = SurfaceConfiguration {
+        //     usage: TextureUsages::RENDER_ATTACHMENT,
+        //     format: self.surface_format,
+        //     width: self.size.width,
+        //     height: self.size.height,
+        //     present_mode: PresentMode::AutoVsync,
+        //     desired_maximum_frame_latency: 2,
+        //     alpha_mode: CompositeAlphaMode::Auto,
+        //     view_formats: vec![self.surface_format.add_srgb_suffix()],
+        // };
+        let config = self
+            .surface
+            .get_default_config(&self.adapter, self.size.width, self.size.height)
+            .unwrap();
 
-        self.surface.configure(&self.device, &surface_config);
+        self.surface.configure(&self.device, &config);
     }
 
     fn resize(&mut self, new_size: PhysicalSize<u32>) {
@@ -73,15 +92,14 @@ impl State {
     fn render(&mut self) {
         // Create texture view
         let surface_texture = self
-            .surface.get_current_texture()
+            .surface
+            .get_current_texture()
             .expect("Failed to acquire next swapchain texture");
 
-        let texture_view = surface_texture
-            .texture
-            .create_view(&TextureViewDescriptor {
-                format: Some(self.surface_format.add_srgb_suffix()),
-                ..Default::default()
-            });
+        let texture_view = surface_texture.texture.create_view(&TextureViewDescriptor {
+            format: Some(self.surface_format.add_srgb_suffix()),
+            ..Default::default()
+        });
 
         // Renders a Green screen
         let mut encoder = self.device.create_command_encoder(&Default::default());
@@ -112,11 +130,88 @@ impl State {
         self.window.pre_present_notify();
         surface_texture.present();
     }
+
+    fn render_triangle(&mut self) {
+        let shader = &self.device.create_shader_module(ShaderModuleDescriptor {
+            label: None,
+            source: wgpu::ShaderSource::Wgsl(Cow::Borrowed(include_str!("triangle_shader.wgsl"))),
+        });
+
+        let pipeline_layout = &self
+            .device
+            .create_pipeline_layout(&PipelineLayoutDescriptor {
+                label: None,
+                bind_group_layouts: &[],
+                push_constant_ranges: &[],
+            });
+
+        let render_pipeline = &self
+            .device
+            .create_render_pipeline(&RenderPipelineDescriptor {
+                label: None,
+                layout: Some(&pipeline_layout),
+                vertex: VertexState {
+                    module: &shader,
+                    entry_point: Some("vs_main"),
+                    compilation_options: Default::default(),
+                    buffers: &[],
+                },
+                primitive: PrimitiveState::default(),
+                depth_stencil: None,
+                multisample: MultisampleState::default(),
+                fragment: Some(FragmentState {
+                    module: &shader,
+                    entry_point: Some("fs_main"),
+                    compilation_options: Default::default(),
+                    targets: &[Some(self.surface_format.into())],
+                }),
+                multiview: None,
+                cache: None,
+            });
+
+        // self.configure_surface();
+
+        let frame = self
+            .surface
+            .get_current_texture()
+            .expect("Failed to acquire next swapchain texture");
+
+        let view = frame.texture.create_view(&TextureViewDescriptor::default());
+
+        let mut encoder = self
+            .device
+            .create_command_encoder(&CommandEncoderDescriptor { label: None });
+
+        {
+            let mut rpass = encoder.begin_render_pass(&RenderPassDescriptor {
+                label: None,
+                color_attachments: &[Some(RenderPassColorAttachment {
+                    view: &view,
+                    depth_slice: None,
+                    resolve_target: None,
+                    ops: Operations {
+                        load: wgpu::LoadOp::Clear(Color::WHITE),
+                        store: wgpu::StoreOp::Store,
+                    },
+                })],
+                depth_stencil_attachment: None,
+                timestamp_writes: None,
+                occlusion_query_set: None,
+            });
+
+            rpass.set_pipeline(&render_pipeline);
+            rpass.draw(0..3, 0..1);
+        }
+
+        self.queue.submit(Some(encoder.finish()));
+        self.window.pre_present_notify();
+        frame.present();
+    }
 }
 
 #[derive(Default)]
 struct App {
-    state: Option<State>
+    state: Option<State>,
 }
 
 impl ApplicationHandler for App {
@@ -124,14 +219,12 @@ impl ApplicationHandler for App {
         let window = Arc::new(
             event_loop
                 .create_window(Window::default_attributes())
-                .unwrap()
+                .unwrap(),
         );
 
-        let state = pollster::block_on(State::new(
-            window.clone()
-        ));
+        let state = pollster::block_on(State::new(window.clone()));
         self.state = Some(state);
-        
+
         window.request_redraw();
     }
 
@@ -148,7 +241,8 @@ impl ApplicationHandler for App {
                 event_loop.exit();
             }
             WindowEvent::RedrawRequested => {
-                state.render();
+                // state.render();
+                state.render_triangle();
                 // Emits a new redraw requested event.
                 state.get_window().request_redraw();
             }
@@ -168,7 +262,7 @@ fn main() {
     let event_loop = EventLoop::new().unwrap();
 
     event_loop.set_control_flow(ControlFlow::Poll);
-    
+
     let mut app = App::default();
     event_loop.run_app(&mut app).unwrap();
 }
